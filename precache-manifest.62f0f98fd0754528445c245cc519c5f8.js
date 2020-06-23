22b66a77e3135d83d08b8a76d980424130344ad8
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-22 17:01:01 -0700

    Updates

---
+++ precache-manifest.62f0f98fd0754528445c245cc519c5f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290829e56637a522b5c37e2d589515ff",
+    "url": "/mattr-laptop-customizer/index.html"
+  },
+  {
+    "revision": "379c9739eb8cb58812ff",
+    "url": "/mattr-laptop-customizer/static/css/main.72a41afd.chunk.css"
+  },
+  {
+    "revision": "5a65be21d8c381a11d89",
+    "url": "/mattr-laptop-customizer/static/js/2.0660a072.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/mattr-laptop-customizer/static/js/2.0660a072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379c9739eb8cb58812ff",
+    "url": "/mattr-laptop-customizer/static/js/main.9d5ae98d.chunk.js"
+  },
+  {
+    "revision": "84f91a9c69ebd39a30ac",
+    "url": "/mattr-laptop-customizer/static/js/runtime-main.2eedf45c.js"
+  }
+]);
